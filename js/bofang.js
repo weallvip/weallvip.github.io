0122ab33b9159e3f91163793935565637ee2b071
weallvip <3433566355@qq.com>
2017-12-02 18:11:50 +0800

    gengxin2.0

--- js/bofang.js
+++ js/bofang.js
@@ -9,17 +9,34 @@
       }
     }   1.1 版本的 js*/
 	
-	
 window.onload = function () {
       var obj = document.getElementById("selectId");
       var url = document.getElementById("addressUrl");
       var videoObj = document.getElementById("player");
 	  var a=['top','left'],b=0; 
       document.getElementById('played').onclick = function () {
-        if (obj.value && url.value) {
-		 videoObj.src = obj.value + url.value;
 
-        }else
+      if (obj.value && url.value) {
+			
+	  var loc = location.href;
+      var n1 = loc.length;//地址的总长度
+      var t = loc.indexOf("=");//取得t号的位置
+	 // var tt = loc.indexOf("tt=");//取得t号的位置
+      var name = decodeURI(loc.substr(t + 1, n1));//从=号后面的内容
+	 // var pass = decodeURI(loc.substr(tt + 3, n1));
+	  //var passtwo = MD5(name);
+		//if (pass != passtwo(name.length, passtwo.length))
+	 //if((name.length + 1) === loc.length)
+		 if(t == -1)
+		{
+			alert("请先登陆！");
+			//alert(name);
+			window.location = '..';
+		}else
+		{
+		   videoObj.src = obj.value + url.value;
+		}
+     }else
 		{
         var u=setInterval(function(){ 
          url.style[a[b%2]]=(b++)%4<2?0:4; 
